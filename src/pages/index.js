377a4220ed423ec84102dbf64ec150673e9ec9e2
Ei <smolbean.ei@gmail.com>
2024-02-15 23:26:09 +0700

    add island

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Home from "./Home";
-import About from "./About";
-import Project from "./Project";
-import Contact from "./Contact";
+import Home from './Home';
+import About from './About';
+import Project from './Project';
+import Contact from './Contact';
 
 export {Home, About, Project, Contact}
